4490009c7c441cfe58a4a75bfd3f1337df3cbc97
samfedenia <sam@fedenia.xyz>
2022-04-08 17:53:27 -0500

    simplify discount formula and reformat output

--- README.md
+++ README.md
@@ -9,3 +9,14 @@
     // automated chromium browser will open ({headless: false} to bypass cloudflare)
     yarn start 
     ```
+#### Example output
+```zsh
+┌──────────────────────────────────┬──────────────┐
+│             (index)              │    Values    │
+├──────────────────────────────────┼──────────────┤
+│ Grayscale ETH Holdings Per Share │ '0.00997456' │
+│          ETH-USD Price           │ '$ 3201.91'  │
+│          ETHE-USD Price          │  '$ 24.48'   │
+│     Grayscale ETHE Discount      │  '23.35 %'   │
+└──────────────────────────────────┴──────────────┘
+```
--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const options = require('./options');
 
 // Driver code
 
-(async ({ URL, sharesSelector, epsSelector }) => {
+(async ({ URL, epsSelector }) => {
   const [page, browser] = await getBrowserPage(puppeteer, URL);
-  const sharesText = await getTextFromSelector(page, sharesSelector);
   const epsText = await getTextFromSelector(page, epsSelector);
-  const shares = parseInt(sharesText.split('‡')[0].replace(',', '').replace(',', ''));
   const eps = parseFloat(epsText.split('‡')[0]);
   await browser.close();
 
   const ethPrice = await getPriceQuote('ETH-USD');
   const ethePrice = await getPriceQuote('ETHE');
-
-  const discount = 1 - ((ethePrice / ethPrice) * shares) / (shares * eps);
-
-  console.log('Grayscale Shares Outstanding: ', shares);
-  console.log('Grayscale ETH Holdings Per Share: ', eps);
-  console.log('ETH-USD Price: ', ethPrice);
-  console.log('ETHE-USD Price: ', ethePrice);
-  console.log('\nGrayscale ETHE Discount: ', (discount * 100).toFixed(2) + '%');
+  const discount = 1 - ethePrice / ethPrice / eps;
+  const data = {
+    'Grayscale ETH Holdings Per Share': eps.toString(),
+    'ETH-USD Price': '$ ' + ethPrice.toFixed(2),
+    'ETHE-USD Price': '$ ' + ethePrice.toFixed(2),
+    'Grayscale ETHE Discount': (discount * 100).toFixed(2) + ' %',
+  };
+  console.table(data);
 })(options);
